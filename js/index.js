de9f37e4a5b5606ed79824958d99c661154cfbfd
AMN2022 <miss.eliseu12@gmail.com>
2022-05-19 20:17:04 +0300

    colorização do fundo do banner

--- js/index.js
+++ js/index.js
@@ -1,26 +1,5 @@
-let img = document.getElementById('img-banner');
-
-indice = 1;
-
-function myFuncion(direcao) {
-    if(direcao == 'posterior'){
-
-        if(indice == 1){
-
-            img.src = "img/img2.jpeg";
-            indice = 2;
-        }
-        else if(indice == 2){
-            img.src = "img/imimg/PicsArt_2.jpg";
-            indice = 1;
-
-        }
-
-
-}
+let img = document.getElementById('img-banner')
+    img.style.backgroundColor = 'orange';
 
 
 
-function trocarimg(){
-    
-}
