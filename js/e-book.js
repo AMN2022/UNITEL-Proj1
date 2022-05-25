0ccc7e1135cc7b298e9f60567c7f7bd18a31c051
AMN2022 <miss.eliseu12@gmail.com>
2022-05-25 08:45:14 +0300

    criando loja online, para ebooks

---
+++ js/e-book.js
@@ -0,0 +1,19 @@
+var produtos = [
+    {
+        imagem
+        nome:"",
+        texto:"",
+    }
+]
+
+produtos.map(produtos){
+    Document.getelementbyId('produtos').innershtml += 
+    {
+        <div class "produtos">
+            <img src="(produto.imagem)">
+            <h2>produto.nome</h2>
+            <p>produto.texto</p>
+            <button>Comprar</button>
+        </div>
+    }
+}
--- js/profeta.js
+++
@@ -1,9 +0,0 @@
-function Enviar() {
-
-    var nome = document.getElementById("formulario");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
-}
